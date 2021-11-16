55b76fa2099e2cf1f62082968b632685e1537f59
Jules <jules.augustin@gmail.com>
2021-11-16 17:27:17 +0100

    modif de modal.css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,11 +11,20 @@ function editNav() {
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
-
+const modalClose = document.querySelector('.close');
 // launch modal event
+// ecoute du click sur les boutons je m'inscris
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
-// launch modal form
+// ecoute du click sur la croix
+modalClose.addEventListener('click', closeModal);
+
+// launch modal form (permet d'afficher le formulaire)
 function launchModal() {
 	modalbg.style.display = 'block';
+}
+
+// function pour fermer le formulaire
+function closeModal() {
+	modalbg.style.display = "none";
 }
