c8926a1c929b12bd17805e925a072df100b4d5a8
Jules <jules.augustin@gmail.com>
2021-11-23 23:40:17 +0100

    modif de model.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,47 @@ function launchModal() {
 function closeModal() {
 	modalbg.style.display = "none";
 }
+// Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+
+//function validateFormData() {
+	// On récupère l'élément sur lequel on veut détecter le clic
+const elt = document.getElementById('bouton');
+// On écoute l'événement click
+elt.addEventListener('click', function (event) {
+
+	event.preventDefault();
+	console.log(document.getElementById('first').value);
+	const prenom = document.getElementById('first').value
+
+});
+//}
+
+
+
+// Les données doivent être saisies correctement :
+// Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+function prenom() {
+
+}
+// Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+function nom() {
+
+}
+// L'adresse électronique est valide.
+function email() {
+
+}
+// Pour le nombre de concours, une valeur numérique est saisie.
+function nombreConcours() {
+
+}
+// Un bouton radio est sélectionné.
+
+// La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+
+function conditionsGenrales() {
+
+}
+// Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+
