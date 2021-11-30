27866381f2fec807a28c99eae6780ce2cccbeb25
Jules <jules.augustin@gmail.com>
2021-11-30 12:42:08 +0100

    modif de model.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,30 +30,35 @@ function closeModal() {
 }
 // Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
 
-//function validateFormData() {
-	// On récupère l'élément sur lequel on veut détecter le clic
+// On récupère l'élément sur lequel on veut détecter le clic
 const elt = document.getElementById('bouton');
 // On écoute l'événement click
 elt.addEventListener('click', function (event) {
-
+// On utilise la fonction preventDefault de notre objet event pour empêcher le comportement par défaut de cet élément lors du clic de la souris
 	event.preventDefault();
 	console.log(document.getElementById('first').value);
 	const prenom = document.getElementById('first').value
-
 });
-//}
-
-
-
+// 30/11/21 - Récupérer toutes les données du formulaire et contrôler les champs avec les expressions régulières
 // Les données doivent être saisies correctement :
 // Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-function prenom() {
-
+function checkLength() {
+if (document.getElementById('first').value.length > 2) {
+	alert('Le champ Prénom a un minimum de 2 caractères')
+	return false;
+}
+else {
+	return true;
 }
 // Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-function nom() {
-
-}
+function checkLength() {
+	if (document.getElementById('last').value.length > 2) {
+		alert('Le champ Prénom a un minimum de 2 caractères')
+		return false;
+	}
+	else {
+		return true;
+	}
 // L'adresse électronique est valide.
 function email() {
 
